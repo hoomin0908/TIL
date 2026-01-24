86ea9ecae1045fe887a0e2cd9b7d9e5bcd855d5e
성호민 <hmsung0908@naver.com>
2026-01-24 22:52:28 +0900

    2026-01-24

---
+++ L4.5/L4.5_Q8.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+
+for i in range(6) :
+    i = i+n
+    print(i)
---
+++ L4.5/L4.5_Q9.py
@@ -0,0 +1,8 @@
+arr = [0]*4
+a,b = map(int,input().split())
+
+arr[0] = a
+arr[2] = b
+
+for i in arr :
+    print(i,end="")
