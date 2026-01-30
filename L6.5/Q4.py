4c48ec107fb10733e86da7eab36dd83cde92cf16
hoomin0908 <hoomin0908@gmail.com>
2026-01-30 16:18:32 +0900

    Delete L6.5 directory

--- L6.5/Q4.py
+++
@@ -1,7 +0,0 @@
-n = input()
-if n.islower() :
-    print('소문자입력!!')
-elif n.isupper() :
-    print('대문자입력!!')
-elif n.isdigit() :
-    print('숫자문자입력!!')
--- L6.5/Q9.py
+++
@@ -1,7 +0,0 @@
-arr = [5,4,1,2,7,8]
-n = int(input())
-
-for i in range(n) :
-    for j in arr[::-1] :
-        print(j,end=" ")
-    print()
